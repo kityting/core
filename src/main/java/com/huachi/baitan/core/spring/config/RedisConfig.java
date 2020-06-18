d65acb66a470ca7c036cceb521828e968fd27771
ela <weiliting2005001@163.com>
2020-06-18 22:03:52 +0800

    redis

---
+++ src/main/java/com/huachi/baitan/core/spring/config/RedisConfig.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2020 Huachi.com All right reserved. This software is the
+ * confidential and proprietary information of Huachi.com ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with Huachi.com.
+ */
+
+package com.huachi.baitan.core.spring.config;
+
+import java.lang.reflect.Method;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * 类RedisConfig的实现描述：redis缓存配置
+ *
+ * @author weiliting 2020年6月18日 PM9:17:08
+ */
+@Configuration
+@EnableCaching
+public class RedisConfig extends CachingConfigurerSupport {
+    /**
+     * 生成key的策略
+     *
+     * @return
+     */
+    @Bean
+    public KeyGenerator keyGenerator() {
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(target.getClass().getName());
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };
+    }
+
+    @Bean
+    public CacheManager CacheManagercacheManager(RedisTemplate<?, ?> redisTemplate) {
+        CacheManager cacheManager = new RedisCacheManager(redisTemplate);
+        return cacheManager;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+        redisTemplate.setConnectionFactory(factory);
+
+        // 修改rdis默认序列化方式为StringRedisSerializer
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+        redisTemplate.setHashValueSerializer(stringRedisSerializer);
+        redisTemplate.setValueSerializer(stringRedisSerializer);
+
+        return redisTemplate;
+    }
+
+}
